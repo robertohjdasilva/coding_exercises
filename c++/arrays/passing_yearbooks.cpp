9576c0c0ccc88ddd85f509472b08a476b7d5f176
Roberto H Jacob Da Silva <robertohjdasilva@hotmail.com>
2020-06-03 15:16:49 -0700

    Add magical candy bags Update passing yearbooks

--- c++/arrays/passing_yearbooks.cpp
+++ c++/arrays/passing_yearbooks.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
 vector <int> findSignatureCounts(vector <int> arr) {
     int size = (int) arr.size ();
     if (size == 0) return {};
@@ -14,4 +18,57 @@ vector <int> findSignatureCounts(vector <int> arr) {
       }
     }
     return signatures;  
+}
+
+void printIntegerVector(vector <int> array) {
+  int size = array.size();
+  cout << "[";
+  for (int i = 0; i < size; i++) {
+    if (i != 0) {
+      cout << ", ";
+    }
+   cout << array[i];
+  }
+  cout << "]";
+}
+
+int test_case_number = 1;
+
+void check(vector <int>& expected, vector <int>& output) {
+  int expected_size = expected.size();
+  int output_size = output.size();
+  bool result = true;
+  if (expected_size != output_size) {
+    result = false;
+  }
+  for (int i = 0; i < min(expected_size, output_size); i++) {
+    result &= (output[i] == expected[i]);
+  }
+  const char* rightTick = u8"\u2713";
+  const char* wrongTick = u8"\u2717";
+  if (result) {
+    cout << rightTick << "Test #" << test_case_number << "\n";
+  }
+  else {
+    cout << wrongTick << "Test #" << test_case_number << ": Expected ";
+    printIntegerVector(expected);
+    cout << " Your output: ";
+    printIntegerVector(output);
+    cout << endl;
+  }
+  test_case_number++;
+}
+
+int main() {
+  int n_1 = 2;
+  vector <int> arr_1{2, 1};
+  vector <int> expected_1{2, 2};
+  vector <int> output_1 = findSignatureCounts(arr_1);
+  check(expected_1, output_1);
+
+  int n_2 = 2;
+  vector <int> arr_2{1, 2};
+  vector <int> expected_2{1, 1};
+  vector <int> output_2 = findSignatureCounts(arr_2);
+  check(expected_2, output_2);
 }
---
+++ c++/heaps/magical_candy_bags.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int maxCandies(vector <int> arr, int k) {
+  int size = (int) arr.size();
+  if (size == 0) return 0;
+  
+  priority_queue<int> candyCountHeap;
+  for (int num : arr) {
+    candyCountHeap.push (num);
+  }
+  
+  int candiesEaten = 0;
+  for (int i = 0; i < k; i++) { // i = 2
+    int maxCount = candyCountHeap.top(); // = 3
+    candiesEaten += maxCount; // = 14
+    candyCountHeap.pop();
+    candyCountHeap.push(floor(maxCount/2));
+  }
+  return candiesEaten;
+}
+
+void printInteger(int n) {
+  cout << "[" << n << "]";
+}
+
+int test_case_number = 1;
+
+void check(int expected, int output) {
+  bool result = (expected == output);
+  const char* rightTick = u8"\u2713";
+  const char* wrongTick = u8"\u2717";
+  if (result) {
+    cout << rightTick << "Test #" << test_case_number << "\n";
+  }
+  else {
+    cout << wrongTick << "Test #" << test_case_number << ": Expected ";
+    printInteger(expected); 
+    cout << " Your output: ";
+    printInteger(output);
+    cout << endl; 
+  }
+  test_case_number++;
+}
+
+
+int main() {
+
+  int n_1 = 5, k_1 = 3;
+  vector <int> arr_1{2, 1, 7, 4, 2};
+  int expected_1 = 14;
+  int output_1 = maxCandies(arr_1, k_1);
+  check(expected_1, output_1);
+
+  int n_2 = 9, k_2 = 3;
+  vector <int> arr_2{19, 78, 76, 72, 48, 8, 24, 74, 29};
+  int expected_2 = 228;
+  int output_2 = maxCandies(arr_2, k_2);
+  check(expected_2, output_2);
+}
