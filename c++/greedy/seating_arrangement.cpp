b69e2d8815487cf992678e9449cde9c7dc81c87b
Roberto H Jacob Da Silva <robertohjdasilva@hotmail.com>
2020-06-18 00:26:51 -0700

    Updated to add greedy solution

--- c++/greedy/seating_arrangement.cpp
+++ c++/greedy/seating_arrangement.cpp
@@ -1,8 +1,22 @@
-#include <bits/stdc++.h>
-
 using namespace std;
 
-// Complexity: Time = O(n! * n), Space = O(1);
+#define GREEDY
+
+// Example 1:
+// Input = [5, 10, 6, 8]
+// Output = [5, 8, 10, 6]
+// awkwardness = 4
+//
+// Example 2:
+// Input = [1 2 5 3 7]
+// Output = [1 3 7 5 2]
+//
+// awkwardness = 4
+//
+// Complexity: 
+// - Permutations: Time = O(n! * n), Space = O(1);
+// - Greedy: Time = O(n ^ 2), SPace = O(1)
+//
 int getAwkwardness (vector<int>& arr) {
   int size = (int) arr.size();
   if (size == 0) {
@@ -17,7 +31,61 @@ int getAwkwardness (vector<int>& arr) {
   return maxAwkward;
 }
 
+int getMinIndex (vector<int>& arr, int left, int right) {
+  int size = (int) arr.size();
+  if (size == 0) {
+    return -1;
+  }
+  int minIndex = left;
+  int minValue = arr[left];
+  for (int i = left; i <= right; i++) {
+    if (arr[i] < minValue) {
+      minValue = arr[i];
+      minIndex = i;
+    }
+  }
+  return minIndex;
+}
+
+void swap(vector<int>& arr, int i, int j) {
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+
 int minOverallAwkwardness(vector <int> arr) {
+#ifdef GREEDY
+  // Greedy algorithm
+  // 1. left = 0, right = size - 1; while (left < right)
+  // 2. Find the minimum element between indexes left and right and swap with arr[left];
+  // 3. Increment left
+  // 4. Find the next minimum element between indexes left and right and swap with arr[right];
+  // 5. Decrement right
+  int size = (int) arr.size();
+  if (size == 0) {
+    return 0;
+  }
+  int left = 0;
+  int right = size - 1;
+  int minIndex;
+  
+  while (left < right) {
+    minIndex = getMinIndex (arr, left, right);
+    if (minIndex == -1) return -1;
+    swap(arr, left, minIndex);
+    left++;
+    if (left < right) {
+      minIndex = getMinIndex (arr, left, right);
+      if (minIndex == -1) return -1;
+      swap(arr, right, minIndex); 
+      right--;
+    }
+  }
+  return getAwkwardness(arr);
+#endif
+  
+#ifdef BRUTE_FORCE  
   // Brute force based on permutations
   int size = (int) arr.size();
   if (size == 0) {
@@ -31,6 +99,7 @@ int minOverallAwkwardness(vector <int> arr) {
   } while (next_permutation (arr.begin(), arr.end())); // O(n!)
 
   return minAwkward;
+#endif  
 }
 
 void printInteger(int n) {
